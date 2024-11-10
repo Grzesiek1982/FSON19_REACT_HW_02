992162178d60dc5396d78ed5022f6ffdd0019c22
Grzesiek1982 <grzesieg1982@interia.pl>
2024-11-11 00:49:40 +0100

    Commit po utworzeniu całego projektu

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+
+import Feedback from "./components/Feedback.jsx";
+import Options from "./components/Options.jsx";
+import Description from "./components/Description.jsx";
+
+const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    const storedFeedback = localStorage.getItem("feedback");
+    return storedFeedback
+      ? JSON.parse(storedFeedback)
+      : {
+          good: 0,
+          neutral: 0,
+          bad: 0,
+        };
+  });
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prev) => ({
+      ...prev,
+      [feedbackType]: prev[feedbackType] + 1,
+    }));
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const positiveFeedback =
+    totalFeedback > 0
+      ? Math.round(((feedback.good + feedback.neutral) / totalFeedback) * 100)
+      : 0;
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  return (
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <p>No feedback yet</p>
+      )}
+    </>
+  );
+};
+
+export default App;
